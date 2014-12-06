4eac14dafc4bd18f71bfe078e93d488c8abfdf51
nmslow243 <nmslow@eagles.usi.edu>
2014-12-05 20:41:06 -0600

    Added TreeDirectory
    Added TreeDirectory

--- TEST/TEST/frmMain.cs
+++ TEST/TEST/frmMain.cs
@@ -17,6 +17,8 @@ namespace TEST
         {
             InitializeComponent();
         }
+        ProgramDir IDEDir;
+        StudentProgram IDEProgram;
         //Instantiate linked list of Statements which can add statement objects to it. 
         LinkedList<Statement> list1 = new LinkedList<Statement>();
         String codeString;
@@ -24,7 +26,7 @@ namespace TEST
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //ProgramDir.Load(treeDirectory);
+            IDEDir = new ProgramDir(tvTreeDirectory);
         }
 
 
@@ -42,7 +44,7 @@ namespace TEST
             Variable varObject = new Variable(varType,varName);
             VarInitStatement varStatement = new VarInitStatement(varObject);
             list1.AddLast(varStatement);
-
+            //IDEProgram.AddStatement(varStatement);
             update_codeOutputBox();
             update_txtOutputBox();
         }
@@ -95,38 +97,39 @@ namespace TEST
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (bChanged == true)
+            NewProgram frmNew = new NewProgram();
+            frmNew.sInitialPath = IDEDir.Path;
+            if (frmNew.ShowDialog(this) == DialogResult.OK)
             {
-                DialogResult Dr = MessageBox.Show("Would you like to save the project?", "Save Project", MessageBoxButtons.YesNoCancel);
-                if (Dr == DialogResult.Yes || Dr == DialogResult.No)
+
+                if (bChanged == true)
                 {
-                    if (Dr == DialogResult.Yes)
+                    DialogResult Dr = MessageBox.Show("Would you like to save the project?", "Save Project", MessageBoxButtons.YesNoCancel);
+                    if (Dr == DialogResult.Yes || Dr == DialogResult.No)
                     {
-                        //Save program
+                        if (Dr == DialogResult.Yes)
+                        {
+                            //Save the file
+                            IDEDir.SaveFile(IDEProgram);
+                            bChanged = false;
+                        }
+                        //Clear code box and exit program
+                        update_codeOutputBox();
+                        update_txtOutputBox();
+                        IDEProgram = new StudentProgram(frmNew.ChoosenLanguage, frmNew.ProgramName, frmNew.Path);
                     }
+                }
+                else
+                {
                     //Clear code box and exit program
                     //Either add empty program to program list or clear program object
-                    list1.Clear();
+                    //list1.Clear();
                     update_codeOutputBox();
                     update_txtOutputBox();
-
-                    //Get new program name
-                    string NewProgName = Microsoft.VisualBasic.Interaction.InputBox( "Enter new program name:","New Program");
-                    update_StatusBar("New Program " + NewProgName + " started.");
+                    IDEProgram = new StudentProgram(frmNew.ChoosenLanguage, frmNew.ProgramName, frmNew.Path);
+                    txtOutputBox.Text = "";
                 }
-            } 
-            /*else
-            {
-                //Clear code box and exit program
-                //Either add empty program to program list or clear program object
-                list1.Clear();
-                update_codeOutputBox();
-                update_txtOutputBox();
-
-                //Get new program name
-                string NewProgName = Microsoft.VisualBasic.Interaction.InputBox("New Program", "Enter new program name:");
-                txtOutputBox.Text = NewProgName;
-            }*/
+            }
 
         }
 
@@ -137,6 +140,16 @@ namespace TEST
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            OpenFileDialog fb = new OpenFileDialog();
+            fb.InitialDirectory = IDEDir.Path;
+            fb.Filter = ".prog";
+            fb.Multiselect = false;
+            fb.Title = "Open Program";
+            fb.AddExtension = true;
+            fb.ShowDialog();
+
+            IDEProgram = IDEDir.LoadFile(fb.FileName);
+            tlsStatementStrip.Enabled = true;
             //Load Program object from serialized file
             //txtCodeBox.Text = Language.Parse(Program);
             //treeDirectory.Select? select correct node
@@ -144,22 +157,30 @@ namespace TEST
 
         private void treeDirectory_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            //Bring up menu? Load Object?
-            if (bChanged == true)
+            if (e.Node.ImageIndex == 1)
             {
-                DialogResult Dr = MessageBox.Show("Would you like to save the project?", "Save Project", MessageBoxButtons.YesNoCancel);
-                if (Dr == DialogResult.Yes || Dr == DialogResult.No)
+                //Bring up menu? Load Object?
+                if (bChanged == true)
                 {
-                    if (Dr == DialogResult.Yes)
+                    DialogResult Dr = MessageBox.Show("Would you like to save the project?", "Save Project", MessageBoxButtons.YesNoCancel);
+                    if (Dr == DialogResult.Yes || Dr == DialogResult.No)
                     {
-                        //Save program
+                        if (Dr == DialogResult.Yes)
+                        {
+                            IDEDir.SaveFile(IDEProgram);
+                            bChanged = false;
+                        }
+                        //Load Object
+                        IDEDir.LoadFile(IDEDir.ParentPath + "\\" + e.Node.FullPath);
+                        tlsStatementStrip.Enabled = true;
                     }
-                    //Load Object
                 }
-            }
-            else
-            {
-                //Load Object
+                else
+                {
+                    //Load Object
+                    IDEDir.LoadFile(IDEDir.ParentPath + "\\" + e.Node.FullPath);
+                    tlsStatementStrip.Enabled = true;
+                }
             }
         }
 
@@ -177,7 +198,8 @@ namespace TEST
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            IDEDir.SaveFile(IDEProgram);
+            bChanged = false;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -216,6 +238,21 @@ namespace TEST
         private void update_StatusBar(string s)
         {
             toolStripStatusLabel1.Text = s;
+        }
+
+        private void fIleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            IDEDir.moveUp();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            IDEDir.LoadTree();
         }
 
 
